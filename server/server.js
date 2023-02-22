5c9b235196ba2cfae981098700f34d69419c60e7
Georg Mansky-Kummert <georg.mansky-kummert@fh-campuswien.ac.at>
2023-02-22 08:03:07 +0100

    Implementation of exercise 1

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ app.use(express.static(path.join(__dirname, 'files')));
 // Configure a 'get' endpoint for data..
 app.get('/data', function (req, res) {
   // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+  res.send(
+    [{ title: 'Moby-Dick', 'isbn': '0553213113', 'price': { 'value': 4.95, 'currency': '€' } },
+    { title: 'Wind in the Willows', 'isbn': '0517632306', 'price': { 'value': 12.99, 'currency': '€' } },
+    { title: 'Wuthering Heights', 'isbn': '0141439556', 'price': { 'value': 8, 'currency': '€' } }])
 })
 
 app.listen(3000)
